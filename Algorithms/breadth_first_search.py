f1d260da85caafbc52ad629e7361c0309dead68c
James Yu <yujames33@gmail.com>
2017-05-02 03:55:17 -0500

    general improvements
    add boilerplate, comments, more functionality

--- Algorithms/breadth_first_search.py
+++ Algorithms/breadth_first_search.py
@@ -1,12 +1,33 @@
-import graphs
+"""This module provides breadth first search functionality.
+    
+Attributes:
+
+Classes:
+    
+Functions:
+    breadth_first_search
+    
+Todo:
+    track depth
+    track other stuff?
+    
+"""
+
+from graphs import Graph, Node
 
 def breadth_first_search(graph, start):
     """Traverse a graph using BFS.
     
     Args:
         graph (Graph): Graph to traverse. 
-        start (int): Starting node's index.
+        start (Node): Starting node.
     
+    Returns:
+        A dictionary mapping strings to search results.
+        
+        {"depth": int,
+         "reachable_nodes": Node list"}
+         
     """
     
     # stuff to track
@@ -18,13 +39,31 @@ def breadth_first_search(graph, start):
     queue.append(start)
     
     # do BFS
-    while(True):
-        if len(queue) > 0:
-            next_node = queue.pop(0)
-            edges = 
-        else:
-            break
+    while(len(queue) > 0):
+        next_node = queue.pop(0)
+        
+        for neighbor in graph.get_edges(next_node):
+            if neighbor not in visited:
+                queue.append(neighbor)
+        
+        visited.append(next_node)
         
     # print results
+    return {"reachable_nodes": visited}
     
 # test
+if __name__ == "__main__":
+    my_graph = Graph()
+    
+    node1 = Node(10, 1)
+    node2 = Node(13, 2)
+    node3 = Node(20, 3)
+    
+    my_graph.add_node(node1)
+    my_graph.add_edge((node2, node3))
+    my_graph.add_edge((node3, node1))
+    
+    my_graph.print_nodes()
+    my_graph.print_edges()
+    
+    print breadth_first_search(my_graph, node2)
--- Custom Libraries/graphs.py
+++ Custom Libraries/graphs.py
@@ -1,3 +1,18 @@
+"""This module provides the required classes for representing graphs.
+    
+Attributes:
+
+Classes:
+    Node
+    Graph
+    
+Functions:
+    
+Todo:
+    Support more graph types.
+    
+"""
+
 from collections import deque
 from copy import deepcopy
 
@@ -25,17 +40,17 @@ class Node:
             
         """
         
-        return (self._data == other._data && self._id == other._id)
+        return (self._data == other._data and self._id == other._id)
         
-    def __hash__(self):
-        """For hashed collections/operations.
-        
-        Returns:
-            int: Hash value.
-            
-        """
-        
-        return hash(self._id)
+    # def __hash__(self):
+    #     """For hashed collections/operations.
+    #     
+    #     Returns:
+    #         int: Hash value.
+    #         
+    #     """
+    #     
+    #     return hash(self._id)
         
     def get_data(self):
         """Get a node's data.
@@ -85,7 +100,7 @@ class Graph:
 
         """
         
-        self._nodes = set()
+        self._nodes = list()
         self._edges = list()
     
     def add_edge(self, new_edge):
@@ -98,8 +113,23 @@ class Graph:
             bool: True if added. False if otherwise.
             
         """
+
+        # arg breakdown
+        from_node = new_edge[0]
+        to_node = new_edge[1]
         
-        None
+        # add nodes if not in nodes container
+        self.add_node(from_node)
+        self.add_node(to_node)
+        
+        # add edge if not in edges container
+        for (start, end) in self._edges:
+            if start == from_node:
+                if to_node not in end:
+                    end.append(to_node)
+                    return True
+                    
+        return False
         
     def add_node(self, new_node):
         """Add a node if its id is unique to the graph's current nodes.
@@ -111,30 +141,32 @@ class Graph:
             bool: True if added. False if otherwise.
         """
         
-        for node in self._nodes:
-            if new_node._id == node._id:
-                return False
-        else:
-            self._nodes.add(new_node)
+        # check presence in nodes list
+        # absense in nodes list implies absense in edges list
+        if new_node not in self._nodes:
+            self._nodes.append(new_node)
+            self._edges.append((new_node, list()))
             return True
+        else:
+            return False
 
-    # def get_edges(self, node):
-    #     """Get a node's neighbors.
-    #     
-    #     Args:
-    #         node (Node): Node whose edges to find.
-    #         
-    #     Returns:
-    #         list: Neighbors of node. None otherwise.
-    #         
-    #     """
-    #     
-    #     for (graph_node, edges) in self._nodes_and_edges:
-    #         if node is graph_node:
-    #             return edges
-    #     else:
-    #         return None
-    #                     
+    def get_edges(self, node):
+        """Get a node's neighbors.
+        
+        Args:
+            node (Node): Node whose edges to find.
+            
+        Returns:
+            list: Neighbors of node. None otherwise.
+            
+        """
+        
+        for (start, end) in self._edges:
+            if start == node:
+                return end
+        else:
+            return None
+                        
     def get_node(self, id):
         """Get a node by id.
         
@@ -147,7 +179,7 @@ class Graph:
         """
         
         for node in self._nodes:
-            if id == node.id:
+            if id == node._id:
                 return node
         else:
             return None
@@ -162,3 +194,23 @@ class Graph:
         
         return self._nodes
     
+    def print_nodes(self):
+        """Print all nodes.
+        
+        """
+        
+        print "~~~Nodes~~~"
+        for node in self._nodes:
+            print "Node id: {}".format(node.get_id())
+            print "  data: {}".format(node.get_data())
+
+    def print_edges(self):
+        """Print all edges.
+        
+        """
+        
+        print "~~~Edges~~~"
+        for (start, end) in self._edges:
+            print "start Node id: {}".format(start.get_id())
+            for end_node in end:
+                print "  end Node id: {}".format(end_node.get_id())
